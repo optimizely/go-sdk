eceba9de4dfbd196c0b102f97245fc67a565dbb8
Mike Ng <mike.ng@optimizely.com>
2019-05-29 21:45:51 -0700

    fix: Bring the default engine back into the main engine file.

--- optimizely/decision/engine.go
+++ optimizely/decision/engine.go
@@ -1,14 +1,22 @@
 package decision
 
-import "github.com/optimizely/go-sdk/optimizely/entities"
+import (
+	"github.com/optimizely/go-sdk/optimizely/entities"
+)
 
-// Engine is used to make a decision for a given feature or experiment
+// Engine interface is used to make a decision for a given feature or experiment
 type Engine interface {
 	GetFeatureDecision(entities.Feature, entities.UserContext) FeatureDecision
 }
 
-// FeatureDecision contains the decision information about a feature
-type FeatureDecision struct {
-	Type           string
-	FeatureEnabled bool
+// DefaultDecisionEngine is used out of the box and can be replaced by the user
+type DefaultDecisionEngine struct {
+}
+
+// GetFeatureDecision returns a decision for the given feature key
+func (*DefaultDecisionEngine) GetFeatureDecision(feature entities.Feature, userContext entities.UserContext) FeatureDecision {
+	enabled := feature.Key != "" && userContext.ID != ""
+	return FeatureDecision{
+		FeatureEnabled: enabled,
+	}
 }
