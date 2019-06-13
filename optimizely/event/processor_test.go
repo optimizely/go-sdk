5d25feb10add555805bf955352eee5c4abcf6525
Thomas Zurkan <thomas.zurkan@optimizely.com>
2019-06-13 09:48:06 -0700

    playing with tests

--- optimizely/event/processor_test.go
+++ optimizely/event/processor_test.go
@@ -1,13 +1,24 @@
 package event
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 
-func TestDefaultEventProcessor_ProcessImpression(*testing.T) {
+func TestDefaultEventProcessor_ProcessImpression(t *testing.T) {
 	processor := NewEventProcessor(100, 100)
 
 	impression := Impression{}
 
 	processor.ProcessImpression(impression)
+
+	result, ok := processor.(*DefaultEventProcessor)
+
+	if ok {
+		assert.Equal(t, result.eventsCount(),1)
+	} else {
+		assert.Equal(t, true, false)
+	}
 
 }
