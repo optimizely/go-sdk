35c2567029a3a21acdb8b408d8fe6f13c5fc3af5
Thomas Zurkan <thomas.zurkan@optimizely.com>
2019-06-13 15:18:41 -0700

    added another unit test

--- optimizely/event/processor_test.go
+++ optimizely/event/processor_test.go
@@ -29,3 +29,32 @@ func TestDefaultEventProcessor_ProcessImpression(t *testing.T) {
 	}
 
 }
+
+func TestDefaultEventProcessor_ProcessImpressions(t *testing.T) {
+	processor := NewEventProcessor(100, 100)
+
+	impression := Impression{}
+
+	processor.ProcessImpression(impression)
+	processor.ProcessImpression(impression)
+
+	result, ok := processor.(*DefaultEventProcessor)
+
+	if ok {
+		assert.Equal(t, 2, result.eventsCount())
+
+		time.Sleep(1000 * time.Millisecond)
+
+		assert.NotNil(t, result.Ticker)
+
+		assert.Equal(t, 1, result.eventsCount())
+
+		time.Sleep(1000 * time.Millisecond)
+
+		assert.Equal(t, 0, result.eventsCount())
+
+	} else {
+		assert.Equal(t, true, false)
+	}
+
+}
