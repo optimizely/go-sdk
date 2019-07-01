e86a5b74b99738cc20d8b922eb4ddc6bd1d5461d
Pawel Szczodruch <pawel.szczodruch@optimizely.com>
2019-07-01 10:41:12 -0700

    added lt, gt, exists matchers

---
+++ optimizely/decision/evaluator/matchers/exists_test.go
@@ -0,0 +1,70 @@
+/****************************************************************************
+ * Copyright 2019, Optimizely, Inc. and contributors                        *
+ *                                                                          *
+ * Licensed under the Apache License, Version 2.0 (the "License");          *
+ * you may not use this file except in compliance with the License.         *
+ * You may obtain a copy of the License at                                  *
+ *                                                                          *
+ *    http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                          *
+ * Unless required by applicable law or agreed to in writing, software      *
+ * distributed under the License is distributed on an "AS IS" BASIS,        *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ * See the License for the specific language governing permissions and      *
+ * limitations under the License.                                           *
+ ***************************************************************************/
+
+package matchers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/optimizely/go-sdk/optimizely/entities"
+)
+
+func TestExistsMatcher(t *testing.T) {
+	matcher := ExistsMatcher{
+		Condition: entities.Condition{
+			Match: "exists",
+			Name:  "string_foo",
+		},
+	}
+
+	// Test match
+	user := entities.UserContext{
+		Attributes: entities.UserAttributes{
+			Attributes: map[string]interface{}{
+				"string_foo": "any_value",
+			},
+		},
+	}
+	result, err := matcher.Match(user)
+	assert.NoError(t, err)
+	assert.True(t, result)
+
+	// Test no match
+	user = entities.UserContext{
+		Attributes: entities.UserAttributes{
+			Attributes: map[string]interface{}{
+				"string_foo1": "not_foo",
+			},
+		},
+	}
+
+	result, err = matcher.Match(user)
+	assert.NoError(t, err)
+	assert.False(t, result)
+
+	// Test null case
+	user = entities.UserContext{
+		Attributes: entities.UserAttributes{
+			Attributes: map[string]interface{}{},
+		},
+	}
+	result, err = matcher.Match(user)
+	assert.NoError(t, err)
+	assert.False(t, result)
+}
+
---
+++ optimizely/decision/evaluator/matchers/gt_test.go
@@ -0,0 +1,161 @@
+/****************************************************************************
+ * Copyright 2019, Optimizely, Inc. and contributors                        *
+ *                                                                          *
+ * Licensed under the Apache License, Version 2.0 (the "License");          *
+ * you may not use this file except in compliance with the License.         *
+ * You may obtain a copy of the License at                                  *
+ *                                                                          *
+ *    http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                          *
+ * Unless required by applicable law or agreed to in writing, software      *
+ * distributed under the License is distributed on an "AS IS" BASIS,        *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ * See the License for the specific language governing permissions and      *
+ * limitations under the License.                                           *
+ ***************************************************************************/
+
+package matchers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/optimizely/go-sdk/optimizely/entities"
+)
+
+
+
+
+func TestGtMatcherInt(t *testing.T) {
+	matcher := GtMatcher{
+		Condition: entities.Condition{
+			Match: "gt",
+			Value: 42,
+			Name:  "int_42",
+		},
+	}
+
+	// Test match - same type
+	user := entities.UserContext{
+		Attributes: entities.UserAttributes{
+			Attributes: map[string]interface{}{
+				"int_42": 43,
+			},
+		},
+	}
+	result, err := matcher.Match(user)
+	assert.NoError(t, err)
+	assert.True(t, result)
+
+	// Test match int to float
+	user = entities.UserContext{
+		Attributes: entities.UserAttributes{
+			Attributes: map[string]interface{}{
+				"int_42": 42.9999,
+			},
+		},
+	}
+
+	result, err = matcher.Match(user)
+	assert.NoError(t, err)
+	assert.True(t, result)
+
+	// Test no match
+	user = entities.UserContext{
+		Attributes: entities.UserAttributes{
+			Attributes: map[string]interface{}{
+				"int_42": 42.00000,
+			},
+		},
+	}
+
+	result, err = matcher.Match(user)
+	assert.NoError(t, err)
+	assert.False(t, result)
+
+
+	// Test no match
+	user = entities.UserContext{
+		Attributes: entities.UserAttributes{
+			Attributes: map[string]interface{}{
+				"int_42": 41,
+			},
+		},
+	}
+
+	result, err = matcher.Match(user)
+	assert.NoError(t, err)
+	assert.False(t, result)
+
+	// Test error case
+	user = entities.UserContext{
+		Attributes: entities.UserAttributes{
+			Attributes: map[string]interface{}{
+				"int_43": 42,
+			},
+		},
+	}
+
+	_, err = matcher.Match(user)
+	assert.Error(t, err)
+}
+
+func TestGtMatcherFloat(t *testing.T) {
+	matcher := GtMatcher{
+		Condition: entities.Condition{
+			Match: "gt",
+			Value: 4.2,
+			Name:  "float_4_2",
+		},
+	}
+
+	// Test match float to int
+	user := entities.UserContext{
+		Attributes: entities.UserAttributes{
+			Attributes: map[string]interface{}{
+				"float_4_2": 5,
+			},
+		},
+	}
+	result, err := matcher.Match(user)
+	assert.NoError(t, err)
+	assert.True(t, result)
+
+	// Test match
+	user = entities.UserContext{
+		Attributes: entities.UserAttributes{
+			Attributes: map[string]interface{}{
+				"float_4_2": 4.29999,
+			},
+		},
+	}
+	result, err = matcher.Match(user)
+	assert.NoError(t, err)
+	assert.True(t, result)
+
+	// Test no match
+	user = entities.UserContext{
+		Attributes: entities.UserAttributes{
+			Attributes: map[string]interface{}{
+				"float_4_2": 4.2,
+			},
+		},
+	}
+
+	result, err = matcher.Match(user)
+	assert.NoError(t, err)
+	assert.False(t, result)
+
+	// Test error case
+	user = entities.UserContext{
+		Attributes: entities.UserAttributes{
+			Attributes: map[string]interface{}{
+				"float_4_3": 4.2,
+			},
+		},
+	}
+
+	_, err = matcher.Match(user)
+	assert.Error(t, err)
+}
