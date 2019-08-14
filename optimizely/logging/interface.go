485e246bdf18b80058b47ae13b5f0902d07395e9
Jerry Yuan Chen <yuan.chen@optimizely.com>
2019-08-14 10:37:36 -0700

    feat(log): Add loglevel to default logger message (#60)

--- optimizely/logging/interface.go
+++ optimizely/logging/interface.go
@@ -2,8 +2,8 @@ package logging
 
 // OptimizelyLogConsumer consumes log messages produced by the log producers
 type OptimizelyLogConsumer interface {
-	Log(level int, message string)
-	SetLogLevel(logLevel int)
+	Log(level LogLevel, message string)
+	SetLogLevel(logLevel LogLevel)
 }
 
 // OptimizelyLogProducer produces log messages to be consumed by the log consumer
