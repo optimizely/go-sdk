33c6ef2ab0bb75df9acfc56073fa986ca36a95c0
Thomas Zurkan <thomas.zurkan@optimizely.com>
2019-08-28 14:53:08 -0700

    make channel queue generic channel to handle multiple types

--- optimizely/event/chan_queue.go
+++ optimizely/event/chan_queue.go
@@ -1,48 +1,25 @@
 package event
 
-var done = make(chan bool)
-
 type ChanQueue struct {
-	ch chan UserEvent
+	ch chan interface{}
 	messages Queue
+	isValid func(value interface{}) bool
 }
 
 // Get returns queue for given count size
 func (i *ChanQueue) Get(count int) []interface{} {
-
-	events := []interface{}{}
-
-	messages := i.messages.Get(count)
-	for _, message := range messages {
-		events = append(events, message)
-	}
-
-	return events
-}
-
-func (i *ChanQueue) publish(userEvent UserEvent) {
-	i.ch <- userEvent
+	return i.messages.Get(count)
 }
 
 // Add appends item to queue
 func (i *ChanQueue) Add(item interface{}) {
-	event, ok := item.(UserEvent)
-	if !ok {
-		// cannot add non-user events
-		return
-	}
-
-	i.publish(event)
+	i.ch <- item
 }
 
 // Remove removes item from queue and returns elements slice
 func (i *ChanQueue) Remove(count int) []interface{} {
-	userEvents := make([]interface{},0, count)
-	events := i.messages.Remove(count)
-	for _,message := range events {
-		userEvents = append(userEvents, message)
-	}
-	return userEvents
+	return i.messages.Remove(count)
+
 }
 
 // Size returns size of queue
@@ -53,7 +30,7 @@ func (i *ChanQueue) Size() int {
 // NewNSQueue returns new NSQ based queue with given queueSize
 func NewChanQueue(queueSize int) Queue {
 
-	ch := make(chan UserEvent)
+	ch := make(chan interface{})
 
 	i := &ChanQueue{ch:ch, messages: NewInMemoryQueue(queueSize)}
 
