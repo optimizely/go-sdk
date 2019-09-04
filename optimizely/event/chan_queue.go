dd50e9bb187050f669b324af86f4cdd8199b3aad
Thomas Zurkan <thomas.zurkan@optimizely.com>
2019-09-04 11:28:44 -0700

    add channel queues to fm-beta

---
+++ optimizely/event/chan_queue.go
@@ -0,0 +1,44 @@
+package event
+
+type ChanQueue struct {
+	ch chan interface{}
+	messages Queue
+}
+
+// Get returns queue for given count size
+func (i *ChanQueue) Get(count int) []interface{} {
+	return i.messages.Get(count)
+}
+
+// Add appends item to queue
+func (i *ChanQueue) Add(item interface{}) {
+	i.ch <- item
+}
+
+// Remove removes item from queue and returns elements slice
+func (i *ChanQueue) Remove(count int) []interface{} {
+	return i.messages.Remove(count)
+
+}
+
+// Size returns size of queue
+func (i *ChanQueue) Size() int {
+	return i.messages.Size()
+}
+
+// NewChanQueue returns new go channel based queue with given in memory queueSize
+func NewChanQueue(queueSize int) Queue {
+
+	ch := make(chan interface{})
+
+	i := &ChanQueue{ch:ch, messages: NewInMemoryQueue(queueSize)}
+
+	go func() {
+		for item := range i.ch {
+			i.messages.Add(item)
+		}
+	}()
+
+	return i
+}
+
