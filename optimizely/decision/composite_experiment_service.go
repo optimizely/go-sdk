93e34de18509a5f88f96a4113df62feba2219369
Matt Carroll <matt.carroll@optimizely.com>
2019-10-04 11:09:16 -0700

    REmove noise change

--- optimizely/decision/composite_experiment_service.go
+++ optimizely/decision/composite_experiment_service.go
@@ -48,6 +48,7 @@ func NewCompositeExperimentService() *CompositeExperimentService {
 
 // GetDecision returns a decision for the given experiment and user context
 func (s CompositeExperimentService) GetDecision(decisionContext ExperimentDecisionContext, userContext entities.UserContext) (ExperimentDecision, error) {
+
 	experimentDecision := ExperimentDecision{}
 
 	// Run through the various decision services until we get a decision
