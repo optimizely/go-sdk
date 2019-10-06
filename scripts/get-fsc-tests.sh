ff7ca76e6a8020415cc62ac45541999d60f119f5
Yasir Ali <yali@folio3.com>
2019-10-06 12:08:57 +0500

    fixes travis.yml.

--- scripts/get-fsc-tests.sh
+++ scripts/get-fsc-tests.sh
@@ -7,7 +7,7 @@ rm -rf $FSC_PATH
 mkdir -p $FSC_PATH
  
 pushd $FSC_PATH && git init && git fetch --depth=1 https://$CI_USER_TOKEN@github.com/optimizely/fullstack-sdk-compatibility-suite ${FSC_BRANCH:-master} && git checkout FETCH_HEAD && popd
-mkdir -p ./features
+mkdir -p ./tests/integration/features
 cp -r ./$FSC_PATH/features/* ./tests/integration/features
 mkdir -p ./fsc-datafiles
 cp -r ./$FSC_PATH/features/support/datafiles/*.json ./fsc-datafiles
