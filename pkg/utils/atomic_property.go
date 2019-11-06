653397f3a078a8e7cc22b9670914b34fbd22d2d4
Thomas Zurkan <thomas.zurkan@optimizely.com>
2019-11-06 15:03:29 -0800

    fix lint errors

--- pkg/utils/atomic_property.go
+++ pkg/utils/atomic_property.go
@@ -1,18 +1,38 @@
+/****************************************************************************
+ * Copyright 2019, Optimizely, Inc. and contributors                        *
+ *                                                                          *
+ * Licensed under the Apache License, Version 2.0 (the "License");          *
+ * you may not use this file except in compliance with the License.         *
+ * You may obtain a copy of the License at                                  *
+ *                                                                          *
+ *    http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                          *
+ * Unless required by applicable law or agreed to in writing, software      *
+ * distributed under the License is distributed on an "AS IS" BASIS,        *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ * See the License for the specific language governing permissions and      *
+ * limitations under the License.                                           *
+ ***************************************************************************/
+
+// Package utils //
 package utils
 
 import "sync"
 
+// AtomicProperty used to wrap a value with a read write lock.
 type AtomicProperty struct {
 	property interface{}
 	lock sync.RWMutex
 }
 
+// Get() gets the property used at creation using a read lock
 func (p *AtomicProperty)Get() interface{} {
 	p.lock.RLock()
 	defer p.lock.RUnlock()
 	return p.property
 }
 
+// Sets() the property after creation using a write lock
 func (p *AtomicProperty)Set(value interface{}) {
 	p.lock.Lock()
 	defer p.lock.Unlock()
