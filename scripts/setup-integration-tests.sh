60a83379384953d5b223e3f69acc93ad4fb481e3
Yasir Ali <yali@folio3.com>
2019-12-02 12:34:34 +0500

    fixes.

--- scripts/setup-integration-tests.sh
+++ scripts/setup-integration-tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+. ~/.nvm/nvm.sh
 mkdir -p $FSC_PATH
 pushd $FSC_PATH && git init && git fetch --depth=1 https://$CI_USER_TOKEN@github.com/optimizely/fullstack-sdk-compatibility-suite ${FSC_BRANCH:-master} && git checkout FETCH_HEAD
 ln -s features/support/datafiles/ public 
