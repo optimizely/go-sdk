ecf1d16205543cc2f7ec76b6d0fbc089d253a114
Tom Zurkan <thomas.zurkan@optimizely.com>
2020-04-22 15:12:06 -0700

    prepare for 1.1.3 release (#255)

--- pkg/event/version.go
+++ pkg/event/version.go
@@ -18,7 +18,7 @@
 package event
 
 // Version is the current version of the client
-var Version = "1.1.1"
+var Version = "1.1.3"
 
 // ClientName is the name of the client
 var ClientName = "go-sdk"
