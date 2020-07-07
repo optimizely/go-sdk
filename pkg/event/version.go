ca2840025695e44b66e3323bec3f0aee539d467c
Matt Carroll <matt.carroll@optimizely.com>
2020-07-07 10:36:10 -0700

    chore: Prepare 1.3.0 release

--- pkg/event/version.go
+++ pkg/event/version.go
@@ -18,7 +18,7 @@
 package event
 
 // Version is the current version of the client
-var Version = "1.3.0-beta3"
+var Version = "1.3.0"
 
 // ClientName is the name of the client
 var ClientName = "go-sdk"
