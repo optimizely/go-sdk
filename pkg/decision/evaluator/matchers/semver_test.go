e89d07d000ee1bd65c17936a1932b5911ea59ce5
Pawel Szczodruch <44238966+pawels-optimizely@users.noreply.github.com>
2020-08-20 09:46:04 -0700

    Update pkg/decision/evaluator/matchers/semver_test.go
    Co-authored-by: Mike Davis <mike@optimizely.com>

--- pkg/decision/evaluator/matchers/semver_test.go
+++ pkg/decision/evaluator/matchers/semver_test.go
@@ -24,13 +24,93 @@ import (
 	"github.com/optimizely/go-sdk/pkg/entities"
 )
 
-type test struct {
-	name      string
-	version   interface{}
-	attribute string
+func TestValidAttributes(t *testing.T) {
+	scenarios := []struct{
+		matchType string
+		version   string
+		expected    bool
+	}{
+		{matchType: "semver_eq", version: "2.0.0", expected: true},
+		{matchType: "semver_eq", version: "2.9", expected: false},
+		{matchType: "semver_eq", version: "1.9", expected: false},
 
-	validBool bool
-	result    bool
+		{matchType: "semver_ge", version: "2.0.0", expected: true},
+		{matchType: "semver_ge", version: "2.9", expected: true},
+		{matchType: "semver_ge", version: "1.9", expected: false},
+
+		{matchType: "semver_gt", version: "2.0.0", expected: false},
+		{matchType: "semver_gt", version: "2.9", expected: true},
+		{matchType: "semver_gt", version: "1.9", expected: false},
+
+		{matchType: "semver_le", version: "2.0.0", expected: true},
+		{matchType: "semver_le", version: "2.9", expected: false},
+		{matchType: "semver_le", version: "1.9", expected: true},
+
+		{matchType: "semver_lt", version: "2.0.0", expected: false},
+		{matchType: "semver_lt", version: "2.9", expected: false},
+		{matchType: "semver_lt", version: "1.9", expected: false},
+	}
+
+	for _, scenario := range scenarios {
+		condition := entities.Condition{
+			Match: scenario.matchType,
+			Value: "2.0",
+			Name:  "version",
+		}
+		user := entities.UserContext{
+			Attributes: map[string]interface{}{
+				"version": scenario.version,
+			},
+		}
+
+		messageAndArgs := []interface{}{"matchType: %s, condition: %s, attribute: %s", scenario.matchType, condition.Value, scenario.version}
+
+		matcher, ok := Get(scenario.matchType)
+		assert.True(t, ok, messageAndArgs...)
+
+		actual, err := matcher(condition, user)
+		assert.NoError(t, err, messageAndArgs...)
+
+		assert.Equal(t, scenario.expected, actual, messageAndArgs...)
+	}
+}
+
+func TestInvalidAttributes(t *testing.T) {
+
+	condition := entities.Condition{
+		Match: "semver_eq",
+		Value: "2.0",
+		Name:  "version",
+	}
+
+	matchTypes := []string{
+		"semver_eq",
+		"semver_ge",
+		"semver_gt",
+		"semver_le",
+		"semver_lt",
+	}
+
+	invalidAttributes := []interface{}{
+		true,
+		37,
+		nil,
+	}
+
+	for _, matchType := range matchTypes {
+		matcher, ok := Get(matchType)
+		assert.True(t, ok)
+
+		for _, attribute := range invalidAttributes {
+			user := entities.UserContext{
+				Attributes: map[string]interface{}{
+					"version": attribute,
+				},
+			}
+			_, err := matcher(condition, user)
+			assert.Error(t, err, "matchType: %s, value: %v", matchType, attribute)
+		}
+	}
 }
 
 func TestSemverEqMatcher(t *testing.T) {
