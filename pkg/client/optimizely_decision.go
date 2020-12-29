00721153f2ad3fe97652c7c51846d5796ef519dd
Pawel Szczodruch <44238966+pawels-optimizely@users.noreply.github.com>
2020-12-29 09:52:00 -0800

    refact: Changes needed for agent (#305)

--- pkg/client/optimizely_decision.go
+++ pkg/client/optimizely_decision.go
@@ -23,69 +23,34 @@ import (
 
 // OptimizelyDecision defines the decision returned by decide api.
 type OptimizelyDecision struct {
-	variationKey string
-	enabled      bool
-	variables    *optimizelyjson.OptimizelyJSON
-	ruleKey      string
-	flagKey      string
-	userContext  OptimizelyUserContext
-	reasons      []string
+	VariationKey string                         `json:"variationKey"`
+	Enabled      bool                           `json:"enabled"`
+	Variables    *optimizelyjson.OptimizelyJSON `json:"-"`
+	RuleKey      string                         `json:"ruleKey"`
+	FlagKey      string                         `json:"flagKey"`
+	UserContext  OptimizelyUserContext          `json:"userContext"`
+	Reasons      []string                       `json:"reasons"`
 }
 
 // NewOptimizelyDecision creates and returns a new instance of OptimizelyDecision
 func NewOptimizelyDecision(variationKey, ruleKey, flagKey string, enabled bool, variables *optimizelyjson.OptimizelyJSON, userContext OptimizelyUserContext, reasons []string) OptimizelyDecision {
 	return OptimizelyDecision{
-		variationKey: variationKey,
-		enabled:      enabled,
-		variables:    variables,
-		ruleKey:      ruleKey,
-		flagKey:      flagKey,
-		userContext:  userContext,
-		reasons:      reasons,
+		VariationKey: variationKey,
+		Enabled:      enabled,
+		Variables:    variables,
+		RuleKey:      ruleKey,
+		FlagKey:      flagKey,
+		UserContext:  userContext,
+		Reasons:      reasons,
 	}
 }
 
 // NewErrorDecision returns a decision with error
 func NewErrorDecision(key string, user OptimizelyUserContext, err error) OptimizelyDecision {
 	return OptimizelyDecision{
-		flagKey:     key,
-		userContext: user,
-		variables:   &optimizelyjson.OptimizelyJSON{},
-		reasons:     []string{err.Error()},
+		FlagKey:     key,
+		UserContext: user,
+		Variables:   &optimizelyjson.OptimizelyJSON{},
+		Reasons:     []string{err.Error()},
 	}
-}
-
-// GetVariationKey returns variation key for optimizely decision.
-func (o OptimizelyDecision) GetVariationKey() string {
-	return o.variationKey
-}
-
-// GetEnabled returns the boolean value indicating if the flag is enabled or not.
-func (o OptimizelyDecision) GetEnabled() bool {
-	return o.enabled
-}
-
-// GetVariables returns the collection of variables associated with the decision.
-func (o OptimizelyDecision) GetVariables() *optimizelyjson.OptimizelyJSON {
-	return o.variables
-}
-
-// GetRuleKey returns the rule key of the decision.
-func (o OptimizelyDecision) GetRuleKey() string {
-	return o.ruleKey
-}
-
-// GetFlagKey returns the flag key for which the decision was made.
-func (o OptimizelyDecision) GetFlagKey() string {
-	return o.flagKey
-}
-
-// GetUserContext returns the user context for which the  decision was made.
-func (o OptimizelyDecision) GetUserContext() OptimizelyUserContext {
-	return o.userContext
-}
-
-// GetReasons returns an array of error/info/debug messages describing why the decision has been made.
-func (o OptimizelyDecision) GetReasons() []string {
-	return o.reasons
 }
