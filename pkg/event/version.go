98c3a0abe2b275fdf19e02ae62e5956a47794234
Matjaz Pirnovar <matjaz.pirnovar@optimizely.com>
2022-08-29 10:47:18 -0700

    chore: update version and changelog for patch release

--- pkg/event/version.go
+++ pkg/event/version.go
@@ -18,7 +18,7 @@
 package event
 
 // Version is the current version of the client
-var Version = "1.8.0"
+var Version = "1.8.1"
 
 // ClientName is the name of the client
 var ClientName = "go-sdk"
