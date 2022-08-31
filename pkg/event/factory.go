f79a413d3fe78391ffd07fc4252cc0a881ca217b
Yasir Ali <yali@folio3.com>
2022-08-31 22:01:58 +0500

    Covering all basic types.

--- pkg/event/factory.go
+++ pkg/event/factory.go
@@ -298,7 +298,8 @@ func isValidAttribute(value interface{}) bool {
 		return false
 	}
 	switch value.(type) {
-	case string, float64, int, bool:
+	// https://go.dev/tour/basics/11
+	case bool, string, int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, uintptr, float32, float64, complex64, complex128:
 		return true
 	default:
 		return false
