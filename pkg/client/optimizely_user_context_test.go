4b5cb0c1cf1a518ca38c9c9f7eb4941e0eb94662
Yasir Ali <yali@folio3.com>
2022-10-20 15:27:11 +0500

    nit fixes.

--- pkg/client/optimizely_user_context_test.go
+++ pkg/client/optimizely_user_context_test.go
@@ -17,7 +17,7 @@
 package client
 
 import (
-	"os"
+	"io/ioutil"
 	"path/filepath"
 	"sync"
 	"testing"
@@ -46,7 +46,7 @@ type OptimizelyUserContextTestSuite struct {
 func (s *OptimizelyUserContextTestSuite) SetupTest() {
 	doOnce.Do(func() {
 		absPath, _ := filepath.Abs("../../test-data/decide-test-datafile.json")
-		datafile, _ = os.ReadFile(absPath)
+		datafile, _ = ioutil.ReadFile(absPath)
 	})
 	s.eventProcessor = new(MockProcessor)
 	s.eventProcessor.On("ProcessEvent", mock.AnythingOfType("event.UserEvent")).Return(true)
