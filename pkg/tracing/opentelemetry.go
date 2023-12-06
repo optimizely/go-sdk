f230d0b8d5da7dce13e865fec43979de3b4764d0
Pulak Bhowmick <pulak.bhowmick@optimizely.com>
2023-12-06 21:58:45 +0600

    add comments

--- pkg/tracing/opentelemetry.go
+++ pkg/tracing/opentelemetry.go
@@ -8,30 +8,20 @@ import (
 	"go.opentelemetry.io/otel/trace"
 )
 
+// Tracer provides the necessary method to collect telemetry trace data.
+// Tracer should not depend on any specific tool. To make it possible it returns a Span interface.
+type Tracer interface {
+	// StartSpan starts a trace span. Span can be a parent or child span based on the passed context.
+	StartSpan(ctx context.Context, tracerName, spanName string) (context.Context, Span)
+}
+
+// Span interface implements the trace span returned by Tracer.
 type Span interface {
 	End()
 	SetAttibutes(key string, value interface{})
 }
 
-type otelSpan struct {
-	span trace.Span
-}
-
-func (s *otelSpan) SetAttibutes(key string, value interface{}) {
-	s.span.SetAttributes(attribute.KeyValue{
-		Key:   attribute.Key(key),
-		Value: attribute.StringValue(value.(string)),
-	})
-}
-
-func (s *otelSpan) End() {
-	s.span.End()
-}
-
-type Tracer interface {
-	StartSpan(ctx context.Context, tracerName, spanName string) (context.Context, Span)
-}
-
+// otelTracer is an OpenTelemetry implementation of Tracer
 type otelTracer struct {
 	enabled bool
 }
@@ -47,6 +37,22 @@ func (t *otelTracer) StartSpan(pctx context.Context, tracerName, spanName string
 	return ctx, &otelSpan{
 		span: span,
 	}
+}
+
+// otelSpan is an OpenTelemetry Span implementation of Span
+type otelSpan struct {
+	span trace.Span
+}
+
+func (s *otelSpan) SetAttibutes(key string, value interface{}) {
+	s.span.SetAttributes(attribute.KeyValue{
+		Key:   attribute.Key(key),
+		Value: attribute.StringValue(value.(string)),
+	})
+}
+
+func (s *otelSpan) End() {
+	s.span.End()
 }
 
 type NoopTracer struct{}
