152937dd1eaf65fe378116a19c6a9827b0b7c3bb
pulak-opti <pulak.bhowmick@episerver.com>
2024-01-22 17:18:44 +0600

    update `event.Version`

--- pkg/event/version.go
+++ pkg/event/version.go
@@ -18,7 +18,7 @@
 package event
 
 // Version is the current version of the client
-var Version = "2.0.0-beta"
+var Version = "2.0.0"
 
 // ClientName is the name of the client
 var ClientName = "go-sdk"
