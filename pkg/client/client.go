b832799e7d76c83c15e30708f8260980da3eb264
esrakartalOpt <esra.kartal@optimizely.com>
2025-05-22 14:05:31 -0500

    Fix the error on client go

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -234,7 +234,7 @@ func (o *OptimizelyClient) decide(userContext *OptimizelyUserContext, key string
 	ruleKey := featureDecision.Experiment.Key
 
 	if o.notificationCenter != nil {
-		decisionNotification := decision.FlagNotification(key, variationKey, ruleKey, flagEnabled, eventSent, usrContext, variableMap, reasonsToReport, experimentID, variationID)
+		decisionNotification := decision.FlagNotification(key, variationKey, ruleKey, experimentID, variationID, flagEnabled, eventSent, usrContext, variableMap, reasonsToReport)
 		o.logger.Debug(fmt.Sprintf(`Feature %q is enabled for user %q? %v`, key, usrContext.ID, flagEnabled))
 		if e := o.notificationCenter.Send(notification.Decision, *decisionNotification); e != nil {
 			o.logger.Warning("Problem with sending notification")
